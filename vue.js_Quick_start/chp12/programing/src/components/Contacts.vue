1342423d84f5644581e7fcf0cac64ef88c214d50
doo9392 <hackish@hanmail.net>
2017-11-20 17:03:21 +0900

    vue.js_Quick_start
    vue.js_Quick_start

--- vue.js_Quick_start/chp12/programing/src/components/Contacts.vue
+++ vue.js_Quick_start/chp12/programing/src/components/Contacts.vue
@@ -25,15 +25,7 @@
 		methods : {
 			navigate : function(no){
 				if(confirm('상세 정보를 보시겠습니까?')){
-					this.$router.push({
-						name : 'contactbyno',
-						params : {
-							no : no
-						},
-						function(){
-							console.log('/contacts/' + no + '로 이동완료!');
-						}
-					});
+					console.log('/contacts/' + no + '로 이동완료');
 				}
 			}
 		}
