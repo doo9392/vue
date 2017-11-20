798478f8d9e85dd0ad3e98bdcad87596f05d60ac
doo9392 <hackish@hanmail.net>
2017-11-20 16:50:10 +0900

    vue.js_Quick_start
    vue.js_Quick_start

--- vue.js_Quick_start/chp12/programing/src/components/Contacts.vue
+++ vue.js_Quick_start/chp12/programing/src/components/Contacts.vue
@@ -1,14 +1,12 @@
 <template>
-	<div>
-		<h1>연락처</h1>
-		<div class="wrapper">
-			<div class="box" v-for="(c, index) in contacts" :key="c.no">
-				<span @click="navigate(c.no)" style="cursor:pointer;">
-					[{{c.name}}]
-				</span>
-			</div>
-		</div>
-		<router-view></router-view>
+<div>
+	<h1>연락처</h1>
+	<div class="wrapper">
+		<div class="box" v-for="(c, index) in contacts" :key="c.no">
+			<router-link :to="{name:'contactbyno', params:{no:c.no}}">{{c.name}}</router-link>
+	</div>
+	</div>
+	<router-view></router-view>
 	</div>
 </template>
 
@@ -20,21 +18,6 @@
 		data : function(){
 			return {
 				contacts : contactlist.contacts
-			}
-		},
-		methods : {
-			navigate : function(no){
-				if(confirm('상세 정보를 보시겠습니까?')){
-					this.$router.push({
-						name : 'contactbyno',
-						params : {
-							no : no
-						},
-						function(){
-							console.log('/contacts/' + no + '로 이동완료!');
-						}
-					});
-				}
 			}
 		}
 	}
