0f0f69bafa89cc7efcd984c936b1c31834632d1e
doo9392 <hackish@hanmail.net>
2017-12-26 18:25:25 +0900

    Hong-web
    Hong-web

---
+++ hong-web/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div :class="$style.Header">
+    Header
+  </div>
+</template>
+
+<style module>
+  .Header {position:fixed; left:0; top:0; width:100%; height:40px; background-color:#222; color:#fff;}
+</style>
